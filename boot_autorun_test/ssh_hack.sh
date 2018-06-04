4a1493f9d2356d1578d53b107d78ee452465bc6d
bianca <bianca@bianca.amp>
2018-06-04 16:46:44 +0200

    test pty_shell
    debug autostart
    
    debug autorun
    
    debug autorun

--- boot_autorun_test/ssh_hack.sh
+++ boot_autorun_test/ssh_hack.sh
@@ -3,5 +3,9 @@
 # In crontab -e add the following line to startz the script:
 #@reboot /home/debian/Git/GeckoBot/boot_autorun_test/ssh_hack.sh 2>&1 | /home/debian/Git/GeckoBot/boot_autorun_test/timestamp.sh  >> /home/debian/Git/GeckoBot/boot_autorun_test/log/cronlog.log
 
+faketty() {
+    script -qfc "$(printf "%q " "$@")" /dev/null
+}
 
-ssh -i ~/.ssh/rootkey -tt root@134.28.136.51 /home/debian/Git/GeckoBot/boot_autorun_test/start.sh
+
+faketty ssh -i ~/.ssh/rootkey -tt root@134.28.136.51 /home/debian/Git/GeckoBot/boot_autorun_test/start.sh
--- boot_autorun_test/start.sh
+++ boot_autorun_test/start.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -l
 
+
+faketty() {
+    script -qfc "$(printf "%q " "$@")" /dev/null
+}
+
+
+# spawning a bash shell:
+#/bin/bash -i -c "cd /home/debian/Git/GeckoBot/boot_autorun_test/ ; config-pin P9_28 pwm ; config-pin P9_42 pwm; python load_pwm.py ; python autorun_test.py&"
+
+# spawning a python bash shell
+# python -c 'import pty; pty.spawn("/bin/sh")' > cd /home/debian/Git/GeckoBot/boot_autorun_test/ ; config-pin P9_28 pwm ; config-pin P9_42 pwm ; python load_pwm.py ; python autorun_test.py& 
+
+# test
+# python -c 'import pty; pty.spawn("/bin/sh", "ls")'
+#faketty python /home/debian/Git/GeckoBot/boot_autorun_test/autorun_test.py
+# faketty python -c "import sys; print sys.stdout.isatty()" | cat
+
+
+
+
 cd /home/debian/Git/GeckoBot/boot_autorun_test/
 config-pin P9_28 pwm
 config-pin P9_42 pwm

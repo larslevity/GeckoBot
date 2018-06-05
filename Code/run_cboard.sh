f6f7baeb3b63b02982afbfdcc53b1bec92bad54b
BBB <root@beaglebone.localdomain>
2018-06-05 12:01:27 +0000

    helper script
    helper script 2

---
+++ Code/run_cboard.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -l
+
+
+
+
+ssh -i ~/.ssh/key_bianca root@134.28.136.51 /home/debian/Git/GeckoBot/Code/startBBB.sh
---
+++ Code/startBBB.sh
@@ -0,0 +1,4 @@
+cd /home/debian/Git/GeckoBot/Code
+config-pin P9_28 pwm
+config-pin P9_42 pwm
+python server_hardware_controlled.py

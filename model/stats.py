d4e3478a57a6b9ede1e75f07d6428fed8d43cd76
Lars Schiller (AmP) <lars@familysc.de>
2018-06-26 12:39:13 +0200

    profile the kinematic model

---
+++ model/stats.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 12:34:46 2018
+
+@author: AmP
+
+Profiler stats.
+"""
+
+import pstats
+
+p = pstats.Stats('log.txt')
+p.strip_dirs().sort_stats('cumtime').print_stats()

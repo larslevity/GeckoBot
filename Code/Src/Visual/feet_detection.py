b9ede1acbc49f80f4b71b47cdc768c596ab22d3c
bianca <bianca@bianca.amp>
2018-12-12 17:14:56 +0100

    test script for real time feet detection
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    test script for real time feet detection
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug

--- Code/Src/Visual/feet_detection.py
+++ Code/Src/Visual/feet_detection.py
@@ -10,42 +10,44 @@ import numpy as np
 import cv2
 
 
-
-cap = cv2.VideoCapture(0) # Set Capture Device, in case of a USB Webcam try 1, or give -1 to get a list of available devices
-ret, frame = cap.read()
-
 def debug_helper(img, name='img', debug=False):
         if debug:
             cv2.imshow(name, img)
             cv2.waitKey(0)
 
 
-def detect_circles(filename='test.png', debug=False):
-    img = cv2.imread(filename, 1)
+def detect_circles(filename, debug=False):
+    if type(filename) == str:    
+        img = cv2.imread(filename, 1)
+    else:
+        img = filename
     debug_helper(img, debug=debug)
-#    img = cv2.medianBlur(img, 5)
+    img = cv2.medianBlur(img, 5)
     debug_helper(img, debug=debug)
 
     # detect green:
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_red = np.array([0, 60, 60])
-    upper_red = np.array([10, 255, 255])
+    upper_red = np.array([10, 255, 255]) # hsv
     gray = cv2.inRange(img_hsv, lower_red, upper_red)
+    debug_helper(gray, debug=debug)
 
     # Adaptive Guassian Threshold is to detect sharp edges in the Image.
     gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                  cv2.THRESH_BINARY, 11, 3.5)
+    debug_helper(gray, debug=debug)
 
     kernel = np.ones((3, 3), np.uint8)
     gray = cv2.erode(gray, kernel, iterations=1)
     gray = cv2.dilate(gray, kernel, iterations=1)
+    debug_helper(gray, debug=debug)
 
     # detect circles in the image
 #    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=11, minDist=101,  # Works for Picture 1
 #                               param1=1,
 #                               param2=100, minRadius=0, maxRadius=20)    
 
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=13, minDist=101,  # works for 2
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, dp=13, minDist=101,  # works for 2
                                param1=1,
                                param2=100, minRadius=0, maxRadius=20)
 
@@ -77,7 +79,7 @@ def detect_circles(filename='test.png', debug=False):
 
             centers = [None]*4
         else:
-            centers = 'Not 4 circles detected'
+            centers = '{} circles detected'.format(len(list(circles[0, :, 0])))
         # convert the (x, y) coordinates and radius to integers
         circles = np.round(circles[0, :]).astype("int")
         # loop over the (x, y) coordinates and radius of the circles
@@ -98,9 +100,34 @@ def detect_circles(filename='test.png', debug=False):
                 centers[idx_sorted[idx]] = (x, y, r)
             debug_helper(img, debug=debug)
 
-    return centers
+        return centers, img
+    else:
+        return None, None
 
 
 if __name__ == '__main__':
-    print detect_circles(debug=True)
+    try:
+        import picamera
+        import picamera.array
+        import imutils
+
+        import time
+        picam = True
+    except ImportError:
+        picam = False
+        print 'No PiCam Found'
+        img = 'test.png'
+    
+    if picam:
+        with picamera.PiCamera() as camera:
+            camera.resolution=(320, 240)
+            # Start a preview and let the camera warm up for 2 seconds
+            camera.start_preview()
+            time.sleep(1)
+            with picamera.array.PiRGBArray(camera) as stream:
+                camera.capture(stream, format='bgr')
+                img = stream.array
+                img = imutils.resize(img, width=400)
+    
+    print detect_circles(img, debug=True)
     cv2.destroyAllWindows()
---
+++ Code/test_feet_detection.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 12 17:01:48 2018
+
+@author: bianca
+"""
+
+import time
+import cv2
+import imutils
+from imutils.video import FPS
+
+from Src.Visual.PiCamera.PiVideoStream import PiVideoStream
+from Src.Visual import feet_detection as fd
+
+
+
+def main(testtime=100, disp=False):
+
+    vs = PiVideoStream().start()
+    # allow the camera sensor to warmup
+    time.sleep(2.0)
+
+
+    # created a *threaded *video stream, and start the FPS counter
+    print("[INFO] sampling THREADED frames from `picamera` module...")
+    
+
+    start = time.time()
+    fps = FPS().start()
+    # loop over some frames...this time using the threaded stream
+    try:
+        while time.time()-start < testtime:
+            # grab the frame from the threaded video stream and resize it
+            # to have a maximum width of 400 pixels
+            frame = vs.read()
+            frame = imutils.resize(frame, width=400)
+            
+            # detect circles
+            circs, img = fd.detect_circles(frame)
+            print circs
+    
+            # check to see if the frame should be displayed to our screen
+            if disp:
+                if img is not None:
+                    cv2.imshow("Frame", img)
+                else:
+                    cv2.imshow("Frame", frame)
+                cv2.waitKey(1) & 0xFF
+    
+            fps.update()
+    finally:
+        vs.stop()
+    # stop the timer and display FPS information
+    fps.stop()
+    cv2.destroyAllWindows()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+
+if __name__ == "__main__":
+    main(disp=True)
+

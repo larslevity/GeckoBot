d418dc4bc73bb81516f5830be60a9424ae3406d4
bianca <bianca@bianca.amp>
2019-01-15 15:38:24 +0100

    camara make photo.py

---
+++ Code/Src/Visual/PiCamera/make_photo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 15:33:24 2019
+
+@author: bianca
+"""
+
+
+import picamera
+import time
+
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (2592, 1944)
+    # Start a preview and let the camera warm up for 2 seconds
+    camera.start_preview()
+    time.sleep(2)
+    
+    camera.capture(time.strftime('photo_%Y-%m-%d--%H-%M-%S'))

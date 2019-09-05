b99beb5a104db2157719ff59ccf1b53e65d63719
Lars Schiller (AmP) <lars@familysc.de>
2019-09-05 11:02:12 +0200

    new clb pattern

---
+++ Code/Patterns/clb/gnerate_clb_pattern.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 15:58:50 2019
+
+@author: AmP
+"""
+
+import csv
+import numpy as np
+
+
+def generate_pattern(p0, p1, p2, p3, p4, p5, t_move):
+    return [p0, p1, p2, p3, p4, p5, 0, 0, False, False, False, False, t_move]
+
+
+def save_list_as_csv(lis, filename='test.csv'):
+     with open(filename, 'w', newline='') as f:
+#    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(lis)
+
+
+def read_list_from_csv(filename):
+    out = []
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            line = []
+            for val in row:
+                if val == 'True':
+                    line.append(True)
+                elif val == 'False':
+                    line.append(False)
+                else:
+                    line.append(float(val))
+            out.append(line)
+    return out
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    ptrn = []
+    for p in np.arange(0, 1, .01):
+        ptrn.append(generate_pattern(p, 0, 0, p, p, 0, t_move=1.5))
+        ptrn.append(generate_pattern(0, 0, 0, 0, 0, 0, t_move=1.5))
+
+    for p in np.arange(0, 1, .01):
+        ptrn.append(generate_pattern(0, p, p, 0, 0, p, t_move=1.5))
+        ptrn.append(generate_pattern(0, 0, 0, 0, 0, 0, t_move=1.5))
+
+    t = [0] + list(np.cumsum([p[-1] for p in ptrn]))
+    for idx in range(1):
+        p1 = [ptrn[0][idx]]+[p[idx] for p in ptrn]
+        plt.step(t, p1)
+
+    plt.figure()
+    t = list([0]+np.cumsum([p[-1] for p in ptrn]))
+    for idx in range(2):
+        p1 = [p[idx] for p in ptrn]
+        plt.step(t, p1)
+
+    save_list_as_csv(ptrn, 'special.csv')

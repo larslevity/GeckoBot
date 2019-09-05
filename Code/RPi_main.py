e7cfe1978160c1d555fb551f29b47f47fbfbca51
amp RPi <nomail@gmail.com>
2019-09-05 15:34:50 +0100

    change RPi resolution
    debug
    
    debug

--- Code/RPi_main.py
+++ Code/RPi_main.py
@@ -18,7 +18,8 @@ from Src.Visual.PiCamera import pickler
 
 
 #resolution = (640, 480)  # DINA0
-resolution = (1280, 960)  # Halle
+# resolution = (1280, 960)  # Halle
+resolution = (1648, 928)  # Halle
 
 def main(alpha_memory, resolution=resolution):
 #    print("[INFO] warm up camera sensor...")

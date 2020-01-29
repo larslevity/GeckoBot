5566ceb65e553fd7e229100b1a34f5a455aef1cc
Mustapha Mond <mustapha@strangepork.las.tu-harburg.de>
2020-01-29 18:13:44 +0100

    obstacle course created

--- Code/Src/PiCamera/GPG-obstacle_course.py
+++ Code/Src/PiCamera/GPG-obstacle_course.py
@@ -8,17 +8,29 @@ Created on Wed Jan 29 17:40:22 2020
 if __name__ == '__main__':
     import cv2
     import IMGprocessing
+    import numpy as np
 
     fname = 'GPG-obstacle_course_src.jpg'
     frame = cv2.imread(fname, 1)
 
+    xref = [np.nan, np.nan]
+    positions_ = [[0, 70, 70, 0, 0], [0, 0, 70, 70, 120]]
+
+    scale = 112./900
+    positions = [
+            [400+int(y/scale) for y in positions_[1]],
+            [850-int(x/scale) for x in positions_[0]]]
+
+    IMGprocessing.draw_positions(frame, positions, xref, thick=0,
+                                 col=(255, 0, 0))
+
     # measure
-    alpha, eps, positions, xref = IMGprocessing.detect_all(frame)
-    X1 = (positions[0][1], positions[1][1])
-    col = (0, 0, 0)
-    IMGprocessing.draw_positions(frame, positions, xref, thick=2, col=col)
-    img = IMGprocessing.draw_eps(frame, X1, eps, color=col, dist=70, thick=2)
-#        IMGprocessing.draw_pose(frame, alpha, eps, positions, ell0, col=col)
+#    alpha, eps, positions, xref = IMGprocessing.detect_all(frame)
+#    X1 = (positions[0][1], positions[1][1])
+#    col = (0, 0, 0)
+#    IMGprocessing.draw_positions(frame, positions, xref, thick=2, col=col)
+#    IMGprocessing.draw_eps(frame, X1, eps, color=col, dist=70, thick=2)
+#    IMGprocessing.draw_pose(frame, alpha, eps, positions, ell0, col=col)
 
     # correction
 #    alpha_opt, eps_opt, positions_opt = \
@@ -36,7 +48,7 @@ if __name__ == '__main__':
 #    print('alp:\t', [round(opt-ori, 2) for ori, opt in zip(alpha, alpha_opt)])
 #    print('deps:\t', round(eps_opt - eps, 2))
 
-    cv2.imwrite('GPG-obstacle_course.jpg', img)
+    cv2.imwrite('GPG-obstacle_course.jpg', frame)
 
     cv2.imshow('frame', frame)
     cv2.waitKey(0)
